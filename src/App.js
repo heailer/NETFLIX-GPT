4665e88faaf2e0f6dae543135c1990e9f42fd1be
amog123 <heythisisamog@gmail.com>
2024-11-01 18:31:00 +0530

    set up for netflix-gpt using create-react-app and tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline text-blue-800">
+      <h1>hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
